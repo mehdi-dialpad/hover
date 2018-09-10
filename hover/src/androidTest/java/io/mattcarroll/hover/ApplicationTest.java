ceeaee7ef1f3f65c38aaf3d567dc413e04596711
Mehdi Raza <mehdi@dialpad.com>
2018-09-10 09:45:38 -0700

    Remove deprecated class

--- hover/src/androidTest/java/io/mattcarroll/hover/ApplicationTest.java
+++ hover/src/androidTest/java/io/mattcarroll/hover/ApplicationTest.java
@@ -15,11 +15,7 @@
  */
 package io.mattcarroll.hover;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
-
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest {
     public ApplicationTest() {
-        super(Application.class);
     }
 }
